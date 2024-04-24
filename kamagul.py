253439c4a17c5dc728f0867d4b53e688e72ac7e4
RAWRRRR132 <167991612+RAWRRRR132@users.noreply.github.com>
2024-04-24 20:22:49 +0700

    Create kamagul.py

---
+++ kamagul.py
@@ -0,0 +1,18 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "rofl":"tanggapan terhadap lelucon",
+            "sheesh":"sedikit ketidaksetujuan",
+            "creepy":"menakutkan,tidak meyenangkan",
+            "aggro":"untuk menjadi agresif/marah",
+            "key":"value"
+            }
+            
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+  
+if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+    print(meme_dict[word])
+else:
+    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+    print("???")
